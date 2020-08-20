498cee0af3022e1d9ded2e8043ae53fb030dcc36
joseph churchley <jchurchley@pop-os.localdomain>
2020-08-20 19:34:43 +0100

    Various database utils added into database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -10,6 +10,7 @@ import datetime as dt
 
 from datetime import datetime
 from sqlite3 import Error
+from misc_utils import return_today
 
 def initialize_database():
 	"""initialize and return a sqlite database that will appear in the user's
@@ -31,10 +32,72 @@ def initialize_database():
 		except:
 			print(Error)
 
+def store_username():
+	"""Prompt the user for a username if username table is empty
+	and store the username within a sqlite table"""
+
+	create_statement = """
+	CREATE TABLE IF NOT EXISTS user_name
+	(
+	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_name text 
+	);"""
+
+	cursor.execute(create_statement)
+
+	cursor.execute('SELECT * FROM user_name')
+	row = cursor.fetchone()
+
+	if row == None:
+		given_username = input("\nit looks like it's your first time logging in - what's your name? ")
+		insert_statement = f"""
+		INSERT INTO user_name (user_name) VALUES('{given_username}')
+		"""
+		cursor.execute(insert_statement)
+		conn.commit()
+	else:
+		pass
+
+def store_login_date():
+	"""store the user's login date in user_logins table
+	in sqlite database"""
+
+	today = return_today()
+
+	create_statement = """
+	CREATE TABLE IF NOT EXISTS user_logins
+	(
+	login_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	login_date text
+	);"""
+
+	cursor.execute(create_statement)
+
+	insert_statement = f"""
+	INSERT INTO user_logins (login_date) VALUES ('{today}')
+	"""
+	cursor.execute(insert_statement)
+	conn.commit()
+
+def return_name():
+	"""return the user's name stored in user_name table 
+	in sqlite database"""
+
+	conn = initialize_database()
+	cursor = conn.cursor()
+
+
+	cursor.execute('SELECT user_name from user_name')
+	name = cursor.fetchone()
+	return name[0]
+
 def return_last_login():
 	"""Return the date the user last logged into the journal
 	by returning the max date logged in table login_dates"""
 
-	#PLACEHOLDER
-	last_login = dt.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
-	return last_login
+	conn = initialize_database()
+	cursor = conn.cursor()
+
+	cursor.execute('SELECT max(login_date) from user_logins')
+	last_login = cursor.fetchone()
+	return last_login[0]
--- run.py
+++ run.py
@@ -7,15 +7,18 @@
 # until the user quits the program.
 
 from jrnl_utils import create_log, get_input
-from database_utils import initialize_database, return_last_login
+from database_utils import initialize_database, store_username, store_login_date, return_last_login, return_name
 from misc_utils import return_version, return_today, days_between
 
 conn = initialize_database()
+cursor = conn.cursor()
+
 version = return_version()
 today = return_today()
 last_login = return_last_login()
 login_diff = days_between(last_login, today)
-name = 'joe'
+name = return_name()
+
 
 def welcome_user(*args):
 	"""welcome the user back and display basic info (version number,
@@ -23,11 +26,12 @@ def welcome_user(*args):
 	and then ask the user to select an option."""
 
 	print(f"\nwelcome to micro-journal - version {version} - you've logged on at {today}...")
-	print(f"\nyour last login date was {last_login} - {login_diff} days ago!")
-	print(f"\ndon't forget, you can type 'help' for help or 'quit' to exit at any time. happy journalling, {name}!")
+	print(f"\nyour last login date was {last_login} - {login_diff} days ago.")
+	print(f"\ndon't forget, you can type 'help' for help or 'quit' to exit at any time.")
+	print(f"\nhappy journalling, {name}!")
 
 	return get_input()
 	
 
 if __name__ == '__main__':
-	print(welcome_user(conn, version, today, last_login, login_diff, name))
+	print(welcome_user(conn, cursor, version, today, last_login, login_diff, name))
